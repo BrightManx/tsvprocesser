14da6473181398373db547dea1813465037a5eab
Giacomo Cirò <130163233+BrightManx@users.noreply.github.com>
2023-06-21 12:28:56 +0200

    Update process_n_nuc.py

--- process_n_nuc.py
+++ process_n_nuc.py
@@ -1,17 +1,16 @@
 import pandas as pd
 
-def process_n_nuc(tsv):  
-
+def process_n_nuc(tsv, cof = 'Nuclei - Number of Objects'):    
+    
     # Import data
     data = pd.read_csv(tsv, sep='\t')
-
+    
     # Rename columns
     df = data.rename(columns={
         'Result': 'res',
         'Row':'row',
         'Column': 'col',
         'Well': 'well',
-        'Nuclei - Number of Objects': 'n_nuc',
         'Nuclei - Nucleus Area [µm²] - Mean per Well': 'area_mean',
         'Nuclei - Nucleus Area [µm²] - StdDev per Well': 'area_std',
         'Number of Analyzed Fields': 'n_fields'
@@ -24,15 +23,18 @@ def process_n_nuc(tsv):
     # Sort values
     df = df.sort_values(['cell_name', 'cell_number', 'day'])
 
-    # Create final DF
+    # Create header DF
     n1 = 4 * df.cell_number.unique().shape[0]
-    final = df.loc[::n1, ['cell_name', 'day']].sort_values(by=['cell_name', 'day'])
+    header = df.loc[::n1, ['cell_name', 'day']].sort_values(by=['cell_name', 'day'])
+
+    # Transpose accordingly
     a = pd.DataFrame() 
     for cell_name in df.cell_name.unique():
         for day in df.day.unique():
-            b = df.loc[(df.day == day) & (df.cell_name == cell_name), ['n_nuc']].transpose()
+            b = df.loc[(df.day == day) & (df.cell_name == cell_name), [cof]].transpose()
             b.columns = [f'{num}_{i+1}' for num in df.cell_number.unique() for i in range(4) ]
             a = pd.concat([a, b], axis=0, ignore_index=True)
-    final.index = a.index
-    demo = pd.concat([final, a], axis = 1)
+    header.index = a.index
+    demo = pd.concat([header, a], axis = 1)
+    
     return demo
